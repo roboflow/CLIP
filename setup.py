6df36fd25c44847e635e076d192fdde2dc7e73a2
Paul Guerrie <paul@roboflow.com>
2023-08-31 17:41:15 +0000

    Publish `rf_clip` package on pypi

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import pkg_resources
 from setuptools import setup, find_packages
 
 setup(
-    name="clip",
+    name="rf_clip",
     py_modules=["clip"],
     version="1.0",
-    description="",
+    description="CLIP (Contrastive Language-Image Pre-Training) is a neural network trained on a variety of (image, text) pairs. It can be instructed in natural language to predict the most relevant text snippet, given an image, without directly optimizing for the task, similarly to the zero-shot capabilities of GPT-2 and 3. We found CLIP matches the performance of the original ResNet50 on ImageNet “zero-shot” without using any of the original 1.28M labeled examples, overcoming several major challenges in computer vision.",
     author="OpenAI",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
@@ -17,5 +17,5 @@ setup(
         )
     ],
     include_package_data=True,
-    extras_require={'dev': ['pytest']},
+    extras_require={"dev": ["pytest"]},
 )
